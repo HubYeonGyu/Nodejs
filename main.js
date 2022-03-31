c18d886c1f7f2acf9ea97f27a81911cb6b3a23b3
HubYeonGyu <102293047+HubYeonGyu@users.noreply.github.com>
2022-04-01 00:02:08 +0900

    Update main.js

--- main.js
+++ main.js
@@ -31,7 +31,8 @@ function templateList(filelist){  //filelist는 데이터 디렉토리 파일의
   return list;                             //만들어진 list를 return한다.
 }
 
-var app = http.createServer(function(request,response){
+var app = http.createServer(function(request,response){   //34~83 위 6번째 줄에 추가된 control 매개변수로 목적에 맞게 링크를 출력
+                                                          //templateHTML 함수를 호출하는 곳에 control 매개변수로 전달할 링크를 보여주는 HTML 코드를 전달한다.
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
     var pathname = url.parse(_url, true).pathname;
@@ -80,7 +81,7 @@ var app = http.createServer(function(request,response){
         response.writeHead(200);
         response.end(template);
       });
-    } else if(pathname === '/create_process'){
+    } else if(pathname === '/create_process'){      //84~92 POST 방식으로 전달받은 정보를 출력하고 파일 형태로 저장하기 위한 writeFile()함수 선언
       var body = '';
       request.on('data', function(data){
           body = body + data;
@@ -89,17 +90,17 @@ var app = http.createServer(function(request,response){
           var post = qs.parse(body);
           var title = post.title;
           var description = post.description;
-          fs.writeFile(`data/${title}`, description, 'utf8', function(err){
+          fs.writeFile(`data/${title}`, description, 'utf8', function(err){ //93~95 파일 쓰기를 마치면 페이지를 리다이렉션 해준다. (리다이렉션은 페이지를 이동시키는 기능이다.)
             response.writeHead(302, {Location: `/?id=${title}`});
             response.end();
           })
       });
-    } else if(pathname === '/update'){
+    } else if(pathname === '/update'){                        //98~121 경로가 update일 때 처리하는 else if문을 추가하고 글 내용을 수정하는 form 제공
       fs.readdir('./data', function(error, filelist){
         fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
           var title = queryData.id;
           var list = templateList(filelist);
-          var template = templateHTML(title, list,
+          var template = templateHTML(title, list,             //103~107 바뀌기 전 title값을 hidden 타입의 <input> 태그에 
             `
             <form action="/update_process" method="post">
               <input type="hidden" name="id" value="${title}">
