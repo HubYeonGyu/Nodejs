58eafb4eacd10042089915f1ecae9f0b21dc543d
HubYeonGyu <102293047+HubYeonGyu@users.noreply.github.com>
2022-04-02 17:30:38 +0900

    Update main.js

--- main.js
+++ main.js
@@ -55,9 +55,10 @@ var app = http.createServer(function(request,response){
             var list = templateList(filelist);
             var template = templateHTML(title, list,
               `<h2>${title}</h2>${description}`,
-              `<a href="/create">create</a>
+              `<a href="/create">create</a>               //58~98 글삭제(delete) 버튼 구현
                <a href="/update?id=${title}">update</a>
-              <form action="delete_process" method="post">
+              <form action="delete_process" method="post">   //글 목록 중 하나를 선택했을 때 delete 버튼 표시, 
+                                                             //form 버튼을 이용해 POST 방식으로 처리하고 delete 버튼을 눌렀을 때 delete_process가 삭제 요청을 함
               <input type="hidden" name="id" value="${title}">
               <input type="submit" value="delete">
               </form>`
@@ -123,7 +124,7 @@ var app = http.createServer(function(request,response){
           response.end(template);
         });
       });
-    } else if(pathname === '/update_process'){
+    } else if(pathname === '/update_process'){                                    //126~142 update_process를 받을 수 있는 코드
       var body = '';
       request.on('data', function(data){
           body = body + data;
@@ -133,14 +134,14 @@ var app = http.createServer(function(request,response){
           var id = post.id;
           var title = post.title;
           var description = post.description;
-          fs.rename(`data/${id}`, `data/${title}`, function(error){
-            fs.writeFile(`data/${title}`, description, 'utf8', function(err){
+          fs.rename(`data/${id}`, `data/${title}`, function(error){                //rename 기능을 이용하여 파일 이름 변경 시키는 코드
+            fs.writeFile(`data/${title}`, description, 'utf8', function(err){      //137~139 fs.rename 블록 안에서 내용을 수정하는 처리 구현 
               response.writeHead(302, {Location: `/?id=${title}`});
               response.end();
             })
           });
       });
-    } else if(pathname === '/delete_process'){
+    } else if(pathname === '/delete_process'){  //144~156 글 삭제 기능 코드
         var body = '';
         request.on('data', function(data){
             body = body + data;
@@ -148,8 +149,9 @@ var app = http.createServer(function(request,response){
         request.on('end', function(){
             var post = qs.parse(body);
             var id = post.id;
-            fs.unlink(`data/${id}`, function(error){
-              response.writeHead(302, {Location:'/'});
+            fs.unlink(`data/${id}`, function(error){  //152~155 unlink 함수 기능으로 글 삭제 요청을 처리하는 코드
+              response.writeHead(302, {Location:'/'}); //id값을 이용해 삭제할 파일 경로를 전달하고, 콜백 함수에 파일 삭제 완료 후 처리할 내용을 작성.
+                                                       //삭제가 끝난 다음 사용자를 홈으로 보내기 위해 302 상태 코드에 Location을 `/`로 지정
               response.end();
             })
           });
